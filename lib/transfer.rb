1c60681c6beaf36f89ec588c991d249cf7083bdf
Trevis McGill <trevismcgill25@gmail.com>
2020-08-11 04:11:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,8 +15,8 @@ end
 
 def execute_transaction
   if @amount > @sender.balance || @sender.status == "closed" || @receiver.status == "closed"
-    puts "Transaction rejected. Please check your account balance."
      @status = "rejected"
+     "Transaction rejected. Please check your account balance."
   else
     @sender.balance -= @amount
     @receiver.balance += @amount
