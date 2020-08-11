9fcc89439181759a5c701ba2e308aa92f00283c7
Trevis McGill <trevismcgill25@gmail.com>
2020-08-11 03:48:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,7 @@ attr_accessor :sender, :receiver, :status
 attr_reader
 
 def initialize(transfer, sender, receiver)
-@sender = sender
+@sender = sender.name
 @receiver = receiver
 @status = "pending"
 @transfer_amount
